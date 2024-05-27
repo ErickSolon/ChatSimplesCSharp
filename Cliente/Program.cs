de3684e62855f3a75ee493c3f702cdfca21c51ab
Érick Solon <erick.solon.prog@gmail.com>
2024-05-26 23:27:46 -0300

    primeiro commit

---
+++ Cliente/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+IPEndPoint IPEndPointConfiguration = new(IPAddress.Loopback, 4444);
+
+using (var Cliente = new Socket(IPEndPointConfiguration.AddressFamily, SocketType.Stream, ProtocolType.Tcp)) {
+    Cliente.Connect(IPEndPointConfiguration);
+
+    if (Cliente.Connected)
+    {
+        Console.WriteLine("Conectado!");
+    }
+
+    while (true) {
+        Console.Write("Escreva a mensagem: ");
+        string EnviarPServidor = Console.ReadLine();
+        byte[] EnviarPServidorCodificado = Encoding.UTF8.GetBytes(EnviarPServidor);
+        Cliente.Send(EnviarPServidorCodificado);
+        Console.WriteLine("Aguardando a mensagem...");
+        byte[] BufferRecebido = new byte[1024];
+        var BufferRecebidoResposta = Cliente.Receive(BufferRecebido, SocketFlags.None);
+        var BufferRecebidoRespostaDecodificada = Encoding.UTF8.GetString(BufferRecebido, 0, BufferRecebidoResposta);
+        Console.WriteLine($"Mensagem: {BufferRecebidoRespostaDecodificada}");
+    }
+}
---
+++ Servidor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+IPEndPoint IPEndPointConfiguration = new(IPAddress.Loopback, 4444);
+
+using (var Servidor = new Socket(IPEndPointConfiguration.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+{
+    Servidor.Bind(IPEndPointConfiguration);
+    Servidor.Listen();
+
+    Console.WriteLine("Escutando na porta 4444");
+
+    while (true)
+    {
+        using (var SocketClient = Servidor.Accept())
+        {
+            if (SocketClient.Connected)
+            {
+                Console.WriteLine("Alguém se conectou!");
+            }
+
+            while (true)
+            {
+                byte[] BufferRecebido = new byte[1024];
+                var BufferRecebidoResposta = SocketClient.Receive(BufferRecebido, SocketFlags.None);
+                var BufferRecebidoRespostaDecodificado = Encoding.UTF8.GetString(BufferRecebido, 0, BufferRecebidoResposta);
+                Console.WriteLine($"Mensagem: {BufferRecebidoRespostaDecodificado}");
+                Console.Write("Escreva a mensagem: ");
+                string MensagemPCliente = Console.ReadLine();
+                byte[] MensagemPClienteCodificado = Encoding.UTF8.GetBytes(MensagemPCliente);
+                SocketClient.Send(MensagemPClienteCodificado);
+                Console.WriteLine("Aguardando a mensagem...");
+            }
+        }
+    }   
+}
